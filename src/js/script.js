72d2d86aacbd6b75528a2ef2186d3fa449df667b
Zagidullin Alfir <alfir1991@gmail.com>
2022-04-03 21:38:57 +0300

    lesson-22 map

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,5 @@
+
+//select header
 const selectHeader = document.querySelector('.select__header');
 const selectBody = document.querySelector('.select__body');
 const selectItem = document.querySelectorAll('.select__item');
@@ -24,3 +26,34 @@ selectItem.forEach((item, i) => {
     selectBody.style.maxHeight = null;
   });
 });
+
+
+//maps
+let center = [59.959906306451806,30.280773320098888];
+
+function init() {
+  let map = new ymaps.Map('map-element', {
+    center: center,
+    zoom: 15
+  });
+
+  let playsmark = new ymaps.Placemark(center, {}, {
+    iconLayout: 'default#image',
+    iconImageHref: '../img/map/mark-cart.svg',
+    iconImageSize: [70, 100],
+    iconImageOffset: [-30, -90]
+  });
+
+  map.controls.remove('geolocationControl'); // удаляем геолокацию
+  map.controls.remove('searchControl'); // удаляем поиск
+  map.controls.remove('trafficControl'); // удаляем контроль трафика
+  map.controls.remove('typeSelector'); // удаляем тип
+  map.controls.remove('fullscreenControl'); // удаляем кнопку перехода в полноэкранный режим
+  map.controls.remove('zoomControl'); // удаляем контрол зуммирования
+  map.controls.remove('rulerControl'); // удаляем контрол правил
+  map.behaviors.disable(['scrollZoom']); // отключаем скролл карты
+
+  map.geoObjects.add(playsmark);
+};
+
+ymaps.ready(init);
