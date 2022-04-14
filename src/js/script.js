1106cac30c5d2275e1294bf3554a9f8c069cb22f
Zagidullin Alfir <alfir1991@gmail.com>
2022-04-14 15:26:30 +0300

    lesson-27

--- src/js/script.js
+++ src/js/script.js
@@ -117,3 +117,34 @@ const swiper = new Swiper('.slider', {
   }
 
 });
+
+//iform send
+const form = document.querySelector('.form__elements');
+
+const sendForm = (data) => {
+  return fetch('mail.php', {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: {
+      'Content-Type': 'application/json; charset=UTF-8'
+    }
+  }).then(res => res.json());
+};
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const dataForm = new FormData(form);
+  const user = {};
+
+  dataForm.forEach((val, key) => {
+    user[key] = val;
+  });
+
+  sendForm(user).then(data => {
+    console.log("Письмо отправлено");
+  });
+
+  form.reset();
+
+});
